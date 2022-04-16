fbc44e786b52132a19a23492570a6b9d0b32bf33
The Universe <axelmgrey@outlook.com>
2022-04-15 21:06:06 -0400

    Test

--- Structures/Omen.cs
+++ Structures/Omen.cs
@@ -1,4 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
+﻿
 // Type: SandboxXIV.Structures.Omen
 // Assembly: SandboxXIV, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
 // MVID: 9B1263EC-C2A7-47EB-B77D-B4BE83A58FC3
